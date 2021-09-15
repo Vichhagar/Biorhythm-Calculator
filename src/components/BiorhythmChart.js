98593ee53532a96aeadc2e74f8099bef0032d99e
vichhagar <greenraysvich@gmail.com>
2021-09-15 12:07:42 +0700

    Andrio and Deploy ready

--- src/components/BiorhythmChart.js
+++ src/components/BiorhythmChart.js
@@ -17,9 +17,9 @@ function BiorhythmChart({ birthDate, targetDate }) {
                 <CartesianGrid vertical = {false} strokeDasharray="3 3" />
                 <XAxis dataKey="date" ticks = {[data[3].date, data[15].date, data[28].date]}/>
                 <ReferenceLine x = {data[15].date}/>
-                <Line dot={false} type="monotone" dataKey="physical" stroke="blue"/>
-                <Line dot={false} type="monotone" dataKey="emotional" stroke="red"/>
-                <Line dot={false} type="monotone" dataKey="intellectual" stroke="green"/>
+                <Line dot={false} type="monotone" dataKey="physical" stroke="blue" className="physical"/>
+                <Line dot={false} type="monotone" dataKey="emotional" stroke="red" className="emotional"/>
+                <Line dot={false} type="monotone" dataKey="intellectual" stroke="green" className="intellectual"/>
             </LineChart>
         </ResponsiveContainer>
     
